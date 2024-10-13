f1150c50d04bc6e3e0655a7a1aba62b0f97d7664
friskals <friskasianturi23@gmail.com>
2024-10-13 13:31:32 +0700

    fix: new table for budget categories

---
+++ app/Models/BudgetCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BudgetCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'budget_id'
+    ];
+}
