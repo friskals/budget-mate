aaada2e3c85720fd8413cd80937caea9f8613571
friskals <friskasianturi23@gmail.com>
2024-10-16 22:27:30 +0700

    revert: lost content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,14 @@
             <!-- Page Content -->
             <main>
 {{--                {{ $slot }}--}}
+                <div class="container">
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session()->get('success')}}
+                        </div>
+                    @endif
+                    @yield('content')
+                </div>
             </main>
         </div>
     </body>
