34504c80a4ac4c7b445369cda0e824f42d7f953c
friskals <friskasianturi23@gmail.com>
2024-10-19 14:09:03 +0700

    feat: delete category by user

--- resources/views/frontsite/category/index.blade.php
+++ resources/views/frontsite/category/index.blade.php
@@ -1,24 +1,40 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row justify-content-center">
-        <div class="col-md-8 offset-md-2">
-            <h1 class="text-center my-2">Category Management</h1>
-
-            <div class="card card-default">
-                <div class="card-header">
-                    Todos
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        @foreach($categories as $category)
-                            <li class="list-group-item">
-                                {{$category->name}}
-                                <a class="btn btn-primary btn-sm float-right" href="/todos/{{$category->category_id}}">View</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+   <div class="row justify-content-center">
+       <div class="col-md-8 mt-3">
+           <h1 class="text-center mb-4">Category Management</h1>
+       <table class="table">
+        <thead class="thead-light-lilac">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Category Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Created At</th>
+            <th scope="col">Modified At</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($categories as $category)
+            <tr>
+                <th scope="row">1</th>
+                <td>{{$category->category_id}}</td>
+                <td>{{$category->name}}</td>
+                <td>{{$category->created_at}}</td>
+                <td>{{$category->modified_at}}</td>
+                <td>
+                    <a type="button" class="btn btn-primary">Edit</a>
+                    <a class="btn btn-danger" href="{{route('category.destroy', $category->category_id)}}" onclick="event.preventDefault();
+                                document.getElementById('delete-form-{{ $category->category_id }}').submit();"><i class="bx bx-trash me-1"></i> Delete</a>
+                    <form id="delete-form-{{ $category->category_id }}" action="{{ route('category.destroy', $category->category_id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+      </table>
+   </div>
+</div>
 @endsection
